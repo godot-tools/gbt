c8890cec49865e9a059839f65b92dfbca327abb5
Jon Bonazza <jonbonazza@gmail.com>
2018-06-09 16:17:06 -0700

    Added Blackboard node type

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ BehaviorTrees plugin for the Godot engine.
 This project is _heavily_ inspired by [godot-behavior-tree-plugin](https://github.com/godot-addons/godot-behavior-tree-plugin) by [brandonlamb](https://github.com/brandonlamb).
 
 I have taken his code base and stripped it down and cleaned it up. Henceforth, I will be improving the usability of the plugin and ehancing the workflow to increase development velocity.
+
+
+# Credits
+
+* The `blackboard.png` icon used for the `Blackboard` node type is made by [Vignesh Oviyan](https://flaticon.com/authors/vignesh-oviyan) from [www.flaticon.com](www.flaticon.com).
+* The other icons are provided by the wonderful [Font Awesome](https://fontawesome.com) team.
--- addons/gbt/plugin.gd
+++ addons/gbt/plugin.gd
@@ -5,6 +5,7 @@ extends EditorPlugin
 var dock
 
 func _enter_tree():
+	add_custom_type("Blackboard", "Node", preload("res://addons/gbt/blackboard.gd"), preload("res://addons/gbt/icons/blackboard.png"))
 	add_custom_type("BehaviorTree", "Node", preload("res://addons/gbt/behavior_tree.gd"), preload("res://addons/gbt/icons/tree.png"))
 	add_custom_type("Selector", "Node", preload("res://addons/gbt/selector.gd"), preload("res://addons/gbt/icons/selector.png"))
 	add_custom_type("Sequence", "Node", preload("res://addons/gbt/sequence.gd"), preload("res://addons/gbt/icons/sequence.png"))
@@ -13,6 +14,7 @@ func _enter_tree():
 	add_custom_type("Succeeder", "Node", preload("res://addons/gbt/succeeder.gd"), preload("res://addons/gbt/icons/succeeder.png"))
 	
 func _exit_tree():
+	remove_custom_type("Blackboard")
 	remove_custom_type("BehaviorTree")
 	remove_custom_type("Selector")
 	remove_custom_type("Sequence")
